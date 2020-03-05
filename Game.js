e42795291b688138060b8d064d0016130f2f2cce
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-03-05 14:47:00 +0530

    Add start function to Game.js

--- Game.js
+++ Game.js
@@ -1,9 +1,19 @@
 let board = require('./GameBoard');
 let player = require('./Player');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 class Game {
     gameBoard = new board.GameBoard(['','','','','','','','','']);
     players = [new player.Player("A","X",[]),
                 new player.Player("B","O",[])];
+
+    start() {
+        let input = rl.prompt();
+        console.log("input");
+    }
 
 }
 
