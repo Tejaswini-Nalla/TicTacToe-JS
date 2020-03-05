0a72d099be3c558b2706e75a8aedd807fb329585
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-03-05 14:35:27 +0530

    Add changes to Game class

--- Game.js
+++ Game.js
@@ -1,9 +1,9 @@
-let GameBoard = require('./GameBoard');
-let Player = require('./Player');
+let board = require('./GameBoard');
+let player = require('./Player');
 class Game {
-    gameBoard = new GameBoard.GameBoard(['','','','','','','','','']);
-    players = [new Player.Player("A","X",[]),
-        new Player.Player("B","O",[])];
+    gameBoard = new board.GameBoard(['','','','','','','','','']);
+    players = [new player.Player("A","X",[]),
+                new player.Player("B","O",[])];
 
 }
 
