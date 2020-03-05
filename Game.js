e147ebd93c303b4236713fb422d053963c4e0f68
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-03-05 12:32:20 +0530

    Create Game.js

---
+++ Game.js
@@ -0,0 +1,8 @@
+let classes = require('./GameBoard');
+class Game {
+    constructor(gameBoard, players) {
+        this.gameBoard = gameBoard;
+        this.players = players;
+    }
+}
+
--- GameBoard.js
+++ GameBoard.js
@@ -3,4 +3,12 @@ class GameBoard {
         this.gameBoard = gameBoard;
     }
     winPositions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+    isBoardFilled(){
+        return this.gameBoard.every(cell => cell !== '');
+    }
+}
+
+module.exports = {
+    GameBoard
 }
