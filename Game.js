54f9c121633c1765a54ffcf8e2e4b8b816b57966
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-03-05 12:34:51 +0530

    Add exports in Game.js

--- Game.js
+++ Game.js
@@ -1,8 +1,10 @@
-let classes = require('./GameBoard');
 class Game {
     constructor(gameBoard, players) {
         this.gameBoard = gameBoard;
         this.players = players;
     }
+}
+module.exports = {
+    Game
 }
 
