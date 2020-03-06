bf2db51993330ebe04c840e8012a5dd1e3353ba3
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-03-06 15:43:41 +0530

    Add functions

--- Game.js
+++ Game.js
@@ -1,18 +1,29 @@
 let board = require('./GameBoard');
 let player = require('./Player');
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const prompt = require('prompt-sync')();
 class Game {
-    gameBoard = new board.GameBoard(['','','','','','','','','']);
+    gameBoard = new board.GameBoard(['','','','','','','','',''], false);
     players = [new player.Player("A","X",[]),
                 new player.Player("B","O",[])];
+    currentPlayerIndex = 0;
+
+    changeCurrentPlayerIndex = () => this.currentPlayerIndex - 1;
+    isPositionEmpty = (position) => this.gameBoard.gameBoard[position] === "";
+
+    playerAction(position) {
+        if(this.isPositionEmpty(position)) {
+            this.players[this.currentPlayerIndex].placeOnBoard()
+        }
+    }
 
     start() {
-        let input = rl.prompt();
-        console.log("input");
+        while(!this.gameBoard.isGameOver) {
+            if (!this.gameBoard.isBoardFilled()) {
+                let position = prompt("Enter the position");
+                playerAction(position);
+
+            }
+        }
     }
 
 }
